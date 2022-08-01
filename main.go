455157fbbd6db6f8cc5d353155d16f468c96a93b
Rudiono <adarudiono@gmail.com>
2022-08-01 23:45:08 +0700

    init upload avatar endpoint

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
@@ -31,6 +32,7 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run()
 
